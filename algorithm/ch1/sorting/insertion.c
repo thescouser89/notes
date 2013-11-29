f30c3b9a35d6d2847b5f3cbddbed05e82937255a
Dustin Kut Moy Cheung <thescouser_89@yahoo.co.uk>
2013-11-29 07:58:50 -0500

    ch1 kinda finished

---
+++ algorithm/ch1/sorting/insertion.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * Insertion Sort: O(n^2)
+ */
+void swap(int *i, int *j) {
+    int temp;
+    temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+void insertion_sort(int *items, int n) {
+    int i;
+    int j;
+
+    for (i = 1; i < n; ++i) {
+        j = i;
+        while ((j != 0) && (items[j] < items[j - 1])) {
+            swap(&items[j], &items[j - 1]);
+            --j;
+        }
+    }
+}
+
+void print_array(int *items, int n) {
+    int i;
+    for (i = 0; i < n; ++i) {
+        printf("%d ", items[i]);
+    }
+    printf("\n\n");
+}
+
+int main(void) {
+    int size = 5;
+    int items[] = {10, 8 ,80, 5, -5};
+
+    printf("Unsorted Array\n");
+    print_array(items, size);
+
+    insertion_sort(items, size);
+
+    printf("Sorted Array\n");
+    print_array(items, size);
+
+    return 0;
+}
